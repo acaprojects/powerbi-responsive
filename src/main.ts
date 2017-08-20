03bdae6e00ccc14287434a63a06fcdbe961b7579
Kim Burgess <kim@acaprojects.com>
2017-08-21 01:40:57 +1000

    Make embed function available via window.powerbi

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
+import { embedReport } from './responsive-report';
+
+// Export usage in other modules
 export { embedReport } from './responsive-report';
+
+// ...and extend the window.powerbi object for direct use
+const win = (window as { powerbi?: any});
+win.powerbi = win.powerbi || {};
+win.powerbi.embedResponsiveReport = embedReport;
