bf8e36f4650e737d3be1706733d02d5996bea7e5
Kim Burgess <kim@acaprojects.com>
2017-08-21 08:33:14 +1000

    Neaten up extension of global powerbi object

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { service } from 'powerbi-client';
 import { embedReport } from './responsive-report';
+import { extend } from './utils';
 
 // Export usage in other modules
 export { embedReport } from './responsive-report';
 
 // ...and extend the window.powerbi object for direct use
-const win = (window as { powerbi?: any});
-win.powerbi = win.powerbi || {};
-win.powerbi.embedResponsiveReport = embedReport;
+const api = {
+    embedResponsiveReport: embedReport
+};
+const win = (window as { powerbi?: service.Service});
+win.powerbi = extend(win.powerbi, api);
--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,9 @@
  * will be overridden such that the right-most is favoured.
  */
 export const merge = <T>(...xs: T[]) => Object.assign({}, ...xs) as T;
+
+/**
+ * Given two object create a new (shallow clone) object that combines the
+ * properties of both. Duplicate keys will be overridden in preference of `b`.
+ */
+export const extend = <A, B>(a: A, b: B) => merge<A | B>(a, b) as A & B;
