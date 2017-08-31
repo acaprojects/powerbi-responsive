a63310ada76d164647710d3ff714d2990e554a98
Kim Burgess <kim@acaprojects.com>
2017-08-31 17:47:11 +1000

    Implement responsive page selection

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { service } from 'powerbi-client';
-import { embedReport } from './report-embed';
+import { embedReport } from './report';
 import { extend } from './utils';
 
 // Export usage in other modules
-export { embedReport } from './report-embed';
+export { embedReport } from './report';
 
 // ...and extend the window.powerbi object for direct use
 const api = {
---
+++ src/report.ts
@@ -0,0 +1,113 @@
+import { models, Report, IEmbedConfiguration } from 'powerbi-client';
+import { IFilter } from 'powerbi-models';
+import { embed } from './embedder';
+import { groupPages, PageView } from './page';
+import { merge, bind, compose, anyTrue } from './utils';
+
+/**
+ * A set of functions that may be used to interact with an embedded report.
+ */
+export interface ReportActions {
+    setPage(name: string): Promise<void>;
+    getPages(): Promise<ResponsivePage[]>;
+
+    getFilters(): Promise<IFilter[]>;
+    setFilters(filters: IFilter[]): Promise<void>;
+
+    reload(): Promise<void>;
+
+    setAccessToken(token: string): Promise<void>;
+
+    fullscreen(): void;
+    exitFullscreen(): void;
+}
+
+/**
+ * Wrapper for a set of responsive page layouts.
+ */
+export interface ResponsivePage {
+    name: string;
+    views: PageView[];
+    isActive(): boolean;
+    activate(): Promise<void>;
+}
+
+/**
+ * Base embedder config.
+ */
+const baseConfig: IEmbedConfiguration = {
+    type: 'report',
+    embedUrl: 'https://app.powerbi.com/reportEmbed',
+    tokenType: models.TokenType.Embed,
+    permissions: models.Permissions.Read,
+    settings: {
+        filterPaneEnabled: false,
+        navContentPaneEnabled: false
+    }
+};
+
+/**
+ * Embed a view-only, reponsive report in the specified element.
+ *
+ * The report should be formed with mutliple pages, providing appropriate
+ * layouts for different view sizes.
+ */
+export const embedReport = (id: string, accessToken: string, container: HTMLElement, opts: IEmbedConfiguration = {}) =>
+    embed<Report>(container, merge(baseConfig, opts, {id, accessToken}))
+        .then(registerEvents)
+        .then(bindActions);
+
+/**
+ * Get a list of all pages within the report.
+ */
+const getPages: (report: Report) => () => Promise<ResponsivePage[]> = report => () =>
+    bind(report, report.getPages)()
+        .then(groupPages)
+        .then(viewList => viewList.map(views => ({
+            name: views[0].name,
+            views,
+            isActive: () => anyTrue(views, view => view.isActive()),
+            activate: () => (views.find(v => v.canShow()) || views[0]).activate()
+        })));
+
+/**
+ * Set the page within a responsive report.
+ */
+const setPage = (report: Report) => (name: string) =>
+    getPages(report)()
+        .then(pages => pages.find(p => name === p.name))
+        .then(group => group
+            ? group.activate()
+            : Promise.reject(`Could not find page titled ${name}`));
+
+/**
+ * Expose a set of actions on a reponsive report that are safe to be passed to
+ * the outside world.
+ */
+const bindActions: (report: Report) => ReportActions = report => ({
+    setPage: setPage(report),
+    getPages: getPages(report),
+    getFilters: bind(report, report.getFilters),
+    setFilters: bind(report, report.setFilters),
+    reload: bind(report, report.reload),
+    setAccessToken: bind(report, report.setAccessToken),
+    fullscreen: bind(report, report.fullscreen),
+    exitFullscreen: bind(report, report.exitFullscreen)
+});
+
+/**
+ * Bind to resize events on the view housing a report and switch in alternative
+ * layouts as required.
+ */
+const registerEvents = (report: Report) => {
+    getPages(report)()
+        .then(console.log);
+
+    window.onresize = () => {
+        const width = report.element.clientWidth;
+        const height = report.element.clientHeight;
+        // console.log(`${width} x ${height}`);
+    };
+
+    return report;
+};
